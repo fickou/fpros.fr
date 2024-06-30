1c650876dfdb9ba988aa632c0f9c0eb456cdee34
fickou <daoudafickou@03gmail.com>
2024-06-30 18:05:12 +0100

    premier commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>{{ title }}</h1>
+  <hr>
+  <div v-if="showModal">
+    <ModalComponent :entete='entete' :text='text' @close="toogleMoadal">
+      <template v-slot:liens>
+        <a href="#">Signup now</a>
+        <a href="#">More...</a>
+      </template>
+    </ModalComponent>
+  </div>
+  <button @click="toogleMoadal" class="bouton">Ouvrir le modal</button>
+</template>
+
+<script>
+
+import ModalComponent from './components/MadalComponent.vue'
+
+export default {
+  name: 'App',
+  components:{
+    ModalComponent
+  },
+  data(){
+    return{
+      title : 'Bienvenu dans mon première application vue JS',
+      entete : 'Pré-inscrivez vous maintenant',
+      text : 'Allez dans fpros.fr et entrez votre nom et email',
+      showModal: false,
+    }
+  },
+  methods:{
+    toogleMoadal(){
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  font-size: 4rem;
+}
+.bouton{
+  position: fixed;
+}
+a{
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px 15px;
+  text-decoration: none;
+  margin: 0 30px;
+}
+a:hover{
+  background: #111;
+  color: #fff;
+}
+</style>
---
+++ src/components/MadalComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div @click.self="closeModal" class="modal_bg">
+        <div class="modal">
+            <h1>{{entete}}</h1>
+            <p>{{text}}</p>
+            <div class="actions">
+                <slot name="liens"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['entete','text'],
+        methods: {
+            closeModal(){
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal_bg{
+        background: #afc;
+        width: 100%;
+        padding-top: 0.5px;
+    }
+    .modal{
+        width: 400px;
+        margin: 10px auto;
+        background: #fff;
+        border-radius: 10px;
+    }
+    .modal h1{
+        font-size: 2rem;
+        color: bisque;
+    }
+    p{
+        font-size: 2rem;
+        font-style: italic;
+    }
+</style>
